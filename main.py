d8ac47ad5b16f205e57d79635d1b9cd641f5f2c9
carpenn <carpenn@users.noreply.github.com>
2018-08-07 11:09:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import send_file
 from flask import render_template
 from werkzeug.utils import secure_filename
 import numpy as np
-#import pandas as pd
+import pandas as pd
 
 #from keras.preprocessing import image
 #from keras.applications import xception
